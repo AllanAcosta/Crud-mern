61e3a88fcd2b45f317107995ae90b6a0666c8cdd
unknown <AllanAcosta>
2020-02-12 09:12:57 -0500

    'CrudBackendReady'

---
+++ models/index.js
@@ -0,0 +1,18 @@
+//Archivo principal para el manejo de los modelos
+import Categoria from './categoria';
+import Articulo from './articulo';
+import Usuario from './usuario';
+import Persona from './persona';
+import Producto from './producto';
+import Ingreso from './ingreso';
+import Venta from './venta';
+
+export default {
+    Categoria, 
+    Articulo,
+    Usuario,
+    Persona,
+    Producto,
+    Ingreso,
+    Venta
+}
---
+++ routes/venta.js
@@ -0,0 +1,20 @@
+import routerx from 'express-promise-router';
+import ventaController from '../controllers/ventaController';
+import auth from '../middlewares/auth';
+
+const router = routerx();
+
+router.post('/add', auth.verifyVendedor, ventaController.add);
+router.get('/query', auth.verifyVendedor, ventaController.query);
+router.get('/list', auth.verifyVendedor, ventaController.list);
+router.get('/grafico12Meses', auth.verifyUsuario, ventaController.grafico12Meses);
+
+/*
+router.put('/update', auth.verifyVendedor, ventaController.update);
+router.delete('/remove', auth.verifyVendedor, ventaController.remove);
+*/
+
+router.put('/activate', auth.verifyVendedor, ventaController.activate);
+router.put('/deactivate', auth.verifyVendedor, ventaController.deactivate);
+
+export default router;
